fe16308baf80daa6969f2a3b7ca1fca2a95684dd
The CodingFreakj <pallavidapriya75@gmail.com>
2021-04-24 21:24:24 +0530

    second commit

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBupDegmbzcteoUSKtefDt5E7strUtXyX8",
+  authDomain: "ecommercewebsite-865f2.firebaseapp.com",
+  projectId: "ecommercewebsite-865f2",
+  storageBucket: "ecommercewebsite-865f2.appspot.com",
+  messagingSenderId: "739115696817",
+  appId: "1:739115696817:web:cd0a5cdfb2107d01ac9890",
+};
---
+++ src/firebase/utils.js
@@ -0,0 +1,4 @@
+import { firebaseConfig } from "../firebase/config";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
