2cea40589def9909ac554db628b4a08102e513c0
The CodingFreakj <pallavidapriya75@gmail.com>
2021-05-01 20:10:24 +0530

    build changed

--- src/firebase/utils.js
+++ src/firebase/utils.js
@@ -4,15 +4,7 @@ import { firebaseConfig } from "./config";
 // Add the Firebase products that you want to use
 import "firebase/auth";
 import "firebase/firestore";
-const firebaseConfig = {
-  apiKey: "AIzaSyBupDegmbzcteoUSKtefDt5E7strUtXyX8",
-  authDomain: "ecommercewebsite-865f2.firebaseapp.com",
-  projectId: "ecommercewebsite-865f2",
-  storageBucket: "ecommercewebsite-865f2.appspot.com",
-  messagingSenderId: "739115696817",
-  appId: "1:739115696817:web:cd0a5cdfb2107d01ac9890",
-  measurementId: "G-DJK42E4KTN",
-};
+
 const app = firebase.initializeApp(firebaseConfig);
 // if (process.env.REACT_APP_DEPLOY_ENV='production') {
 //     firebase.initializeApp(productionConfig)
